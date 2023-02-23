42a5698b7a552d7fedb5f921e8a5372ec65e169f
muratdrmz <mrtdrmz@windowslive.com>
2023-02-23 18:30:23 +0100

    end

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// itmes
+// items
 const menu = [
   {
     id: 1,
@@ -116,3 +116,64 @@ const menu = [
 
 const sectionCenter = document.querySelector(".section-center");
 const container = document.querySelector(".btn-container");
+
+// load items
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu);
+  displayMenuButtons();
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems
+    .map(function (item) {
+      return `<article class="menu-item">
+     <img src=${item.img} alt="menu item" class="photo">
+     <div class="item-info">
+      <header>
+       <h4>${item.title}</h4>
+       <h4 class="price">$${item.price}</h4>
+      </header>
+       <p class="item-text">${item.desc}</p>
+     </div>
+    </article>`;
+    });
+    displayMenu=displayMenu.join("");   ;
+  sectionCenter.innerHTML = displayMenu;
+}
+
+  function displayMenuButtons() {
+    const categories = menu.reduce( function(values, item){
+        if (!values.includes(item.category)) {
+          values.push(item.category);
+        }
+        return values;
+      },
+      ["all"]
+    );
+    const categoryBtns = categories
+      .map(function (category) {
+        return `<button class="filter-btn" data-id=${category}>${category}</button>`;
+      })
+      .join("");
+    container.innerHTML = categoryBtns;
+    const filterBtns = document.querySelectorAll(".filter-btn");
+    // filter items
+    filterBtns.forEach(function (btn) {
+      btn.addEventListener("click", function (e){
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter(function (menuItem) {
+          if (menuItem.category === category) {
+            return menuItem
+          }
+        });
+        if (category === "all") {
+          displayMenuItems(menu);
+        } 
+        else {
+          displayMenuItems(menuCategory);
+        }
+      });
+    });
+  }
+
+
